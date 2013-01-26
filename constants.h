00510ed3a2ee03d974e2b0e30d78d1ce62e87c9a
Ignirtoq <Ignirtoq@localhost>
2013-01-26 14:24:02 -0600

    Added constants.h.

---
+++ constants.h
@@ -0,0 +1,23 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <math.h>
+
+/*
+Values in this module are given in the following units
+Mass - Kilograms
+Length - Meters
+Time - Seconds
+Temperature - Kelvin
+*/
+
+#define GRAVITATIONAL_CONSTANT 6.67e-11
+#define MASS_OF_EARTH 5.97e24
+#define RADIUS_OF_EARTH 6371e3
+#define RADIUS_OF_EARTH_SQUARED 4.068e13
+#define ANGULAR_VELOCITY_OF_EARTH 2 * M_PI / (24*3600)
+#define AIR_SPECIFIC_GAS_CONSTANT 287.058
+#define AIR_SURFACE_TEMPERATURE 288.15
+
+
+#endif
