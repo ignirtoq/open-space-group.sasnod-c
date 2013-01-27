891713e98ec90d6ef088f51ff3e02c6b534b879c
Ignirtoq <Ignirtoq@localhost>
2013-01-26 19:00:58 -0600

    Added gravitational force function from old commonMath.d. Still need to move DragForce and BuoyantForce, but those use delegates, so more care is needed.

--- physics.c
+++ physics.c
@@ -1,6 +1,15 @@
 #include <physics.h>
 #include <geometry.h>
+#include <constants.h>
+#include <math.h>
 
 void PhysicalState_Update(PhysicalState *state, VectorObservable force, VectorObservable torque, double dt)
 {
+}
+
+Vector Physics_GetGravitationalForce(Vector pos, double mass)
+{
+	double positionLengthSquared = Vector_GetLengthSquared(pos);
+	double positionLength = sqrt(positionLengthSquared);
+	return Vector_Scale(position, -GRAVITATIONAL_CONSTANT * MASS_OF_EARTH * mass / (positionLengthSquared * positionLength));
 }
